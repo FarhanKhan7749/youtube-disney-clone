021a95fbccc1e10f0d7ab685f6ddd3bc8ad5e1b6
farhan khan <fk828633@gmail.com>
2023-10-23 22:32:43 +0530

    Login UI is done

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,14 @@ const Login = (props) => {
     return (
         <Container>
             <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                    <SingUp>GET ALL THERE</SingUp>
+                    <Description>Get Premier Access to Raya and the Last Dragon for an additional fee
+                        with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                        and The Disney Bundle will increase by $1.</Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+                </CTA>
                 <BgImage />
             </Content>
         </Container>
@@ -42,8 +50,52 @@ position: absolute;
 top: 0;
 right: 0;
 left: 0;
-z-index=-1;
+z-index:-1;
 `;
 
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 650px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+const SingUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color:#0063e5;
+    margin-bottom:12px;
+    letter-spacing: 1.2px;
+    font-size: 18px;
+    padding: 16.5px;
+    border: 1px solid transperent;
+    border-radius: 4px;
+    &:hover{
+        background-color: #0483ee;
+    }
+`
+
+const Description = styled.p`
+    color:hsla(0, 0%, 93.3%, 1.0);
+    font-size:11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`
 export default Login;
