28bc104f7a244a39e6cf13fd664c4059427310f6
farhan khan <fk828633@gmail.com>
2023-11-25 23:39:23 +0530

    retrive data from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,56 @@ import Recommends from "./Recommends";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
 import Trendings from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../featuers/movie/movieSlice";
+import { selectUserName } from "../featuers/users/userSlice";
 
 const Home = (props) => {
-    return(
+    const dispatch = useDispatch();
+    const username = useSelector(selectUserName);
+
+    let recommends = [];
+    let newDisney = [];
+    let originals = [];
+    let trendings = [];
+
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                console.log(recommends);
+                switch (doc.data().type) {
+                    case 'recommend':
+                        recommends = [...recommends,{ id: doc.id, ...doc.data() }];
+                        break;
+                    case 'new':
+                        newDisney = [...newDisney,{ id: doc.id, ...doc.data() }];
+                        // newDisney.push({ id: doc.id, ...doc.data() });
+                        break;
+                    case 'trending':
+                        trendings = [...trendings,{ id: doc.id, ...doc.data() }];
+
+                        // originals.push({ id: doc.id, ...doc.data() });
+                        break;
+                    case 'original':
+                        originals = [...originals,{ id: doc.id, ...doc.data() }];
+                        // trendings.push({ id: doc.id, ...doc.data() });
+                        break;
+                    default:
+                        return null;
+                }
+            });
+        });
+        dispatch(setMovies({
+            recommends: recommends,
+            newDisney: newDisney,
+            original: originals,
+            trendings: trendings,
+        }));
+    }, [username]);
+
+    return (
         <Container>
             <ImgSlider />
             <Viewers />
